407bc8006c73da7aabb6059131c44e68e5fad4db
geethanjali128 <karrageethanjali2003@gmail.com>
2026-05-18 16:43:48 +0530

    replaced previous gemini model

--- lib/actions/general.actions.ts
+++ lib/actions/general.actions.ts
@@ -69,7 +69,7 @@ export const createFeedback = async (params: CreateFeedbackParams) => {
         finalAssessment,
       },
     } = await generateObject({
-      model: google("gemini-2.0-flash-exp"),
+      model: google("gemini-2.5-flash"),
       schema: feedbackSchema,
       prompt: `
         You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
